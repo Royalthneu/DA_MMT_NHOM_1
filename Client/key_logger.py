303d7c97e774d561500fa3c99b3507639c26bce9
23880092_Nguyễn Thế Trung <93558319+Royalthneu@users.noreply.github.com>
2024-11-01 23:18:15 +0700

    Update key_logger.py

--- Client/key_logger.py
+++ Client/key_logger.py
@@ -5,30 +5,24 @@ from pynput import keyboard
 keylogger_running = False
 
 def key_logger(client_socket):
-    print("Connected to the server. Listening for keystrokes...")
-    print("Press 'Esc' to stop the key logger or type 'exit' and press Enter.")
+    print("Connected to the server. Listening for keystrokes... (Press 'Esc' to stop)")
 
     def on_press(key):
+        # Stop the keylogger locally and notify the server when 'Esc' is pressed
         if key == keyboard.Key.esc:
             client_socket.sendall("STOP_KEY_LOGGER".encode())
             print("Sent command to stop keylogger.")
-            return False  # Stop the listener
+            return False
         else:
-            print(f'Key {key} pressed')  # For debugging;
+            print(f'Key {key} pressed')  # For debugging
 
     with keyboard.Listener(on_press=on_press) as listener:
         try:
             while True:
-                user_input = input()  # Wait for user input
-                if user_input.strip().lower() == "exit":
-                    client_socket.sendall("STOP_KEY_LOGGER".encode())
-                    print("Sent command to stop keylogger.")
-                    break
-
-                 # Nhận dữ liệu từ server
-                data = client_socket.recv(1024)  # Adjust buffer size as needed
+                # Nhận dữ liệu từ server
+                data = client_socket.recv(1024)
                 decoded_data = data.decode("utf-8")
-                
+
                 # Kiểm tra và không in thông báo nếu là lệnh KEYLOGGER_STOPPED
                 if decoded_data == "KEYLOGGER_STOPPED":
                     print("Keylogger stopped by server. Returning to main menu...")
@@ -37,7 +31,7 @@ def key_logger(client_socket):
                 if not data:
                     print("No more data received. Exiting...")
                     break
-                print(data.decode(), end='')  # Print received keystrokes
+                print(decoded_data, end='')  # Print received keystrokes
         except Exception as e:
             print(f"Error receiving data: {e}")
 
@@ -54,3 +48,4 @@ def toggle_key_logger(client_socket):
         client_socket.sendall("STOP_KEY_LOGGER".encode())
         print("Stopping keylogger...")
         keylogger_running = False
+
